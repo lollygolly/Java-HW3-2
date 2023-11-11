f711deaed7d224d91215cd02657dbdf3a2c929ed
LollyGolly <valentina_96@inbox.ru>
2023-11-11 21:12:17 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate(double height, double weight) {
+        double bmi1 = weight / (height * height);
+        int bmi;
+        return bmi = (int) bmi1;
+    }
+}
